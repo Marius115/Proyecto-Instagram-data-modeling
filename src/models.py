8cf3192c249e2ffbfc02ce122d722105edcd56d8
Marius115 <cascaranomeister@gmail.com>
2022-07-03 16:37:04 +0000

    :feelsgood:Diagrama terminado, falta el borrador.

--- src/models.py
+++ src/models.py
@@ -8,23 +8,56 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    # Notice that each column is also a normal Python instance attribute.
+    email = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    # Notice that each column is also a normal Python instance attribute.
+    user_id = Column(Integer, ForeignKey("user.id"))
+    location_id = Column(Integer, ForeignKey("location.id"))
+    description = Column(String(250), nullable=False)
+    caption = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    
+
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table address.
+    id = Column(Integer, primary_key=True)
+    # Notice that each column is also a normal Python instance attribute.
+    user_id = Column(Integer, ForeignKey("user.id"))
+    post_id = Column(Integer, ForeignKey("post.id"))
+
+class Location(Base):
+    __tablename__ = 'location'
+    # Here we define columns for the table address.
+    id = Column(Integer, primary_key=True)
+    # Notice that each column is also a normal Python instance attribute.
+    name = Column(String(250), nullable=False)
+    shortname = Column(String(250), nullable=False)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table person
+    id = Column(Integer, primary_key=True)
+    # Notice that each column is also a normal Python instance attribute.
+    post_id = Column(Integer, ForeignKey("post.id"))
+    photo_id = Column(Integer, nullable=False)
+    emojis = Column(String(250))
+    content = Column(String(250), nullable=False)
+
+
+
 
     def to_dict(self):
         return {}
